eb4c4d7f657b259032e676d5c708db5e35ed62f7
Tungstend <hanjinlong197@gmail.com>
2025-02-09 18:51:08 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,13 +62,6 @@ android {
         }
     }
 
-//    externalNativeBuild {
-//        cmake {
-//            path("src/main/cpp/CMakeLists.txt")
-//            version = "3.22.1"
-//        }
-//    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
