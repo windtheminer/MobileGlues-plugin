150a96136fc4f24b8637c30306d4628819c3f360
Swung0x48 <swung0x48@outlook.com>
2025-02-19 13:21:04 +0800

    [misc] (...): Version bump to 1.0.4 in build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1031
-        versionName = "1.0.3.1"
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
