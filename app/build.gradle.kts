800ba4eea73feb958672d5fda164e085ca63f47b
YouFeng11 <youfeng11@outlook.com>
2025-02-28 20:28:49 +0800

    将release构建的isMinifyEnabled设为true

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
